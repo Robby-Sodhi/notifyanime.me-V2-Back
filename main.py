e708265335ed62ea2fbb7fab8c31ae797829ed29
Robby Sodhi <42954808+Robstersgaming@users.noreply.github.com>
2021-06-18 22:31:18 -0400

    various changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+import DataBase
+import MyAnimeList
+
+from flask import Flask, request, g
 from flask_cors import CORS, cross_origin
-from DataBase import DB
 import base64
 import psycopg2
 import json
@@ -10,6 +12,11 @@ app = Flask(__name__)
 cors = CORS(app)
 #*******************
 # add logging for the amount of logins and dashboard visits!!!!!!!!!!
+def get_db():
+    if "db" not in g:
+        g.db = DataBase.DataBase()
+    return g.db
+
 
 @app.route("/authenticateUser", methods=["POST"])
 def authenticateUser():
@@ -20,11 +27,11 @@ def authenticateUser():
         password = requestData["password"]
         if not username or not password:
             return json.dumps(data_object)
-        elif (DB.verify_user(username, password)):
+        elif (get_db().verify_user(username, password)):
             data_object["status"] = True;
             session_key = secrets.token_urlsafe(128)
             #assume its safe because we already verified user
-            DB.write_session_to_user(username, session_key)
+            get_db().write_session_to_user(username, session_key)
             data_object["session-key"] = session_key
             return json.dumps(data_object)
         else:
@@ -34,20 +41,23 @@ def authenticateUser():
 def getWatchList():
     dataObject = {"sessionKeyValid": False, "WatchList": None}
     session_key = request.headers.get("session-key")
-    if not session_key:
+    if not session_key or not get_db().is_session_valid(session_key):
         return dataObject
-    mal_auth_details = DB.get_mal_auth_details(session_key)
+    mal_auth_details = get_db().get_mal_auth_details(session_key)
     if not mal_auth_details["status"]:
         return dataObject
     else:
-        dataObject["sessionKeyValid"] = True
-        dataObject["WatchList"] = False
+        dataObject["sessionKeyValid"] = get_db().is_session_valid(session_key)
+        dataObject["WatchList"] = MyAnimeList.get_watch_list(mal_auth_details["accesstoken"], mal_auth_details["refreshtoken"])
         return dataObject
-        #make a mal api class and get watch list....
 @app.route("/authenticateMal", methods=["POST"])
 def authenticateMal():
-    print(request.data)
-    return json.dumps({"status": True})
+    data_object = {"status": False}
+    data = json.loads(request.data)
+    if not "sessionKey" in data or not "authorizationCode" in data or not data["sessionKey"] or not data["authorizationCode"] or not get_db().is_session_valid(data["sessionKey"]):
+        return json.dumps(data_object)
+    data_object["status"] = MyAnimeList.authenticate_user(data["sessionKey"], data["authorizationCode"], data["codeChallenge"], get_db())
+    return json.dumps(data_object)
 
 
 if __name__ == "__main__":
